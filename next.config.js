02bd9969a07bbccdc38de52ad26137283bbd3b09
Ashmoore <theodore.daniel@epitech.eu>
2024-02-22 18:02:47 +0100

    Fix npm run build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const withNextIntl = require('next-intl/plugin')();
 
 /** @type {import('next').NextConfig} */
-const config = {};
+const config = {
+  distDir: 'build',
+  experimental: {
+    // This is experimental but can
+    // be enabled to allow parallel threads
+    // with nextjs automatic static generation
+    workerThreads: false,
+    cpus: 4
+  }
+};
 
 module.exports = withNextIntl(config);
