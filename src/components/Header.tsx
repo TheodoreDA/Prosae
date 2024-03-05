42d3db5fea0b9511e719704114b4351e73899e5c
Ashmoore <theodore.daniel@epitech.eu>
2024-03-05 15:48:40 +0100

    Change style of Header (padding, height, font-size)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,18 +13,18 @@ export default function Header(): JSX.Element {
   const messages = useMessages();
 
   return (
-    <div className="flex flex-row justify-between py-10 max-h-header min-h-header">
-      <Image className="ml-32" src="logo-placeholder.svg" width="100" height="10" alt='prosae-logo' />
-      <div className='h-20 flex flex-row items-center'>
+    <div className="flex flex-row justify-between py-5 max-h-header min-h-header">
+      <Image className="ml-32" src="/logo-placeholder.svg" width="100" height="10" alt='prosae-logo' />
+      <div className='flex flex-row items-center text-2xl'>
         <NavigationLink href="/">{t('home')}</NavigationLink>
         <NavigationLink href="/who_am_i">{t('whoAmI')}</NavigationLink>
         <NavigationLink href="/correction">{t('correction')}</NavigationLink>
         <NavigationLink href="/interpretariat">{t('interpretariat')}</NavigationLink>
         <NavigationLink href="/contact">{t('contact')}</NavigationLink>
         <div className="mr-10">
-        <NextIntlClientProvider messages={pick(messages, 'LocaleSwitcher')}>
-          <LocaleSwitcher />
-        </NextIntlClientProvider>
+          <NextIntlClientProvider messages={pick(messages, 'LocaleSwitcher')}>
+            <LocaleSwitcher />
+          </NextIntlClientProvider>
         </div>
       </div>
     </div>
