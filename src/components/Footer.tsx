5a874a66eb656e347eff280bc0cfbf173feb9016
Ashmoore <theodore.daniel@epitech.eu>
2024-03-11 21:57:12 +0100

    Rework of the Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,8 @@ import {
   useMessages,
   useTranslations,
 } from 'next-intl';
+import Image from 'next/image';
+import NavigationLink from './NavigationLink';
 
 export default function Footer(): JSX.Element {
   const messages = useMessages();
@@ -11,42 +13,44 @@ export default function Footer(): JSX.Element {
   const tNav = useTranslations('Navigation');
 
   return (
-    <div className="bg-primary h-footer">
-      <div>
-        <div className="flex flex-row justify-between">
+    <div className="bg-primary h-footer flex flex-col px-32 pt-10">
+      <div className='flex flex-row justify-center text-xl font-light'>
           <NextIntlClientProvider messages={pick(messages, 'Footer')}>
-            <div className="ml-20">
-              {t('quickLinks')}
-              <br />
-              {tNav('home')}
-              <br />
-              {tNav('whoAmI')}
-              <br />
-              {tNav('correction')}
-              <br />
-              {tNav('interpretariat')}
-              <br />
-              {tNav('contact')}
+            <div className='flex flex-col pr-80 space-y-1'>
+              <div className='text-2xl font-semibold pb-3'>{t('quickLinks')}</div>
+              <NavigationLink className="w-fit" href={'/'}>{tNav('home')}</NavigationLink>
+              <NavigationLink className="w-fit" href={'/who_am_i'}>{tNav('whoAmI')}</NavigationLink>
+              <NavigationLink className="w-fit" href={'/correction'}>{tNav('correction')}</NavigationLink>
+              <NavigationLink className="w-fit" href={'/interpretariat'}>{tNav('interpretariat')}</NavigationLink>
+              <NavigationLink className="w-fit" href={'/contact'}>{tNav('contact')}</NavigationLink>
             </div>
           </NextIntlClientProvider>
-          <div className="mr-20">
-            Contactez-moi !<br />
-            <a href="mailto:jeanne.sabiron@prosae.net">
-              jeanne.sabiron@prosae.net
-            </a>
-            <br />
-            +33 6 07 04 50 87
-            <br />
-            <a href="https://instagram.com/_prosae_">@_prosae_</a>
-            <br />
-            <a href="https://www.linkedin.com/company/prosae/">prosae</a>
-          </div>
-        </div>
-        <div className="flex flex-row justify-between border-black border-t-2">
-          <div>@2024 Prosae all rights reserved</div>
-          <div>Terms of use | Privacy Environmental Policy</div>
+        <div className='flex flex-col pl-80 space-y-1'>
+          <div className='text-2xl font-semibold pb-3'>{t('contactMe')}</div>
+          <a className='w-fit' href="mailto:jeanne.sabiron@prosae.net">jeanne.sabiron@prosae.net</a>
+          <a href="tel:+33607045087" className='w-fit'>+33 6 07 04 50 87</a>
+          <a href="https://instagram.com/_prosae_" className='flex flex-row items-center w-fit'>
+            <div className='w-5 h-5 relative mr-2'>
+              <Image src={"/logo-instagram.png"} fill alt='Logo Instagram' />
+            </div>
+            Instagram
+          </a>
+          <a href="https://linkedin.com/company/prosae" className='flex flex-row items-center w-fit'>
+            <div className='w-5 h-5 relative mr-2'>
+              <Image src={"/logo-linkedin.png"} fill alt='Logo Linkedin' />
+            </div>
+            Linkedin
+          </a>
         </div>
       </div>
+      <div className='flex flex-row justify-between border-t border-black mt-10 pt-8 mx-40'>
+          <div>@2024 Prosae all rights reserved</div>
+          <div className='flex flex-row'>
+            <a href='/'>Terms of use</a>
+            <div className='mx-2'>|</div>
+            <a href='/'>Privacy Environmental Policy</a>
+          </div>
+      </div>
     </div>
-  );
+    );
 }
