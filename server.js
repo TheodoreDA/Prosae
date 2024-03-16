10aef4db42ec6c4b4bc300cebea27194d3e44a62
Ashmoore <theodore.daniel@epitech.eu>
2024-03-16 17:57:07 +0100

    Adding env domain and port with default values

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const { parse } = require('url')
 const next = require('next')
  
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = 5432
+const hostname = process.env.URL_DOMAIN ?? 'localhost'
+const port = process.env.PORT ?? 3000
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
