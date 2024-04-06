639715c56836f471352bbe790c3d925d7b542a1c
Ashmoore <theodore.daniel@epitech.eu>
2024-04-06 17:29:10 +0200

    Readding the development banner

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -39,7 +39,7 @@ export default function LocaleLayout({ params, children }: LayoutProps) {
         )}
       >
         <Header />
-        {/* <BannerUnderDevelopment /> */}
+        <BannerUnderDevelopment />
         <div className="min-h-body overflow-y-scroll">
           {children}
           <Footer />
