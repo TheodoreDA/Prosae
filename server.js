97fce8e406bf0230a97acaeb8c0b8246013ae5fd
Ashmoore <theodore.daniel@epitech.eu>
2024-04-06 17:38:26 +0200

    Better error handling

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.prepare().then(() => {
         await handle(req, res, parsedUrl)
       }
     } catch (err) {
-      console.error('Error occurred handling', req.url, err)
+      console.log('Error occurred handling', req.url, err)
       res.statusCode = 500
       res.end('internal server error')
     }
