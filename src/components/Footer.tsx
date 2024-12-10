7365e950a91708d529624cb30e7a9d9e7e435798
Ashmoore <theodore.daniel@epitech.eu>
2024-12-10 21:52:22 +0100

    Adding responsiveness to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,25 +13,25 @@ export default function Footer(): JSX.Element {
   const tNav = useTranslations('Navigation');
 
   return (
-    <div className="h-footer max-w-std-screen w-full bg-primary px-32 pt-10">
-      <div className='flex flex-row justify-between text-xl mx-20'>
-          <NextIntlClientProvider messages={pick(messages, 'Footer')}>
-            <div>
-              <div className='text-2xl font-semibold'>{t('quickLinks')}</div>
-              <div className='grid gap-x-5 gap-y-1 grid-cols-2 pt-3'>
-                <NavigationLink className="w-fit" href={'/'}>{tNav('home')}</NavigationLink>
-                <NavigationLink className="w-fit" href={'/interpretariat'}>{tNav('interpretariat')}</NavigationLink>
-                <NavigationLink className="w-fit" href={'/who_am_i'}>{tNav('whoAmI')}</NavigationLink>
-                <a className='w-fit inline-block transition-all duration-300 ease-in-out hover:scale-90 transform-origin-center' href="https://prosae.odoo.com">{tNav('blog')}</a>
-                <NavigationLink className="w-fit" href={'/biography'}>{tNav('biography')}</NavigationLink>
-                <NavigationLink className="w-fit" href={'/contact'}>{tNav('contact')}</NavigationLink>
-                <NavigationLink className="w-fit" href={'/correction'}>{tNav('correction')}</NavigationLink>
-              </div>
+    <div className="max-w-std-screen w-full bg-primary px-5 sm:px-10 md:px-20 lg:px-32 pt-8">
+      <div className='flex flex-col sm:flex-row max-w-72 sm:max-w-std-screen justify-between text-lg sm:text-xl mx-4 lg:mx-20'>
+        <NextIntlClientProvider messages={pick(messages, 'Footer')}>
+          <div>
+            <div className='text-xl sm:text-2xl font-semibold'>{t('quickLinks')}</div>
+            <div className='grid gap-x-5 gap-y-1 grid-cols-2 pt-2 sm:pt-4'>
+              <NavigationLink className="w-24 sm:w-fit" href={'/'}>{tNav('home')}</NavigationLink>
+              <NavigationLink className="w-24 sm:w-fit" href={'/interpretariat'}>{tNav('interpretariat')}</NavigationLink>
+              <NavigationLink className="w-24 sm:w-fit" href={'/who_am_i'}>{tNav('whoAmI')}</NavigationLink>
+              <a className='w-24 sm:w-fit inline-block transition-all duration-300 ease-in-out hover:scale-90 transform-origin-center' href="https://prosae.odoo.com">{tNav('blog')}</a>
+              <NavigationLink className="w-24 sm:w-fit" href={'/biography'}>{tNav('biography')}</NavigationLink>
+              <NavigationLink className="w-24 sm:w-fit" href={'/contact'}>{tNav('contact')}</NavigationLink>
+              <NavigationLink className="w-24 sm:w-fit" href={'/correction'}>{tNav('correction')}</NavigationLink>
             </div>
-          </NextIntlClientProvider>
-        <div className='flex flex-col space-y-1'>
-          <div className='text-2xl font-semibold'>{t('contactMe')}</div>
-          <a className='w-fit pt-3' href="mailto:jeanne.sabiron@prosae.net">jeanne.sabiron@prosae.net</a>
+          </div>
+        </NextIntlClientProvider>
+        <div className='flex flex-col space-y-1 pt-8 sm:pt-0'>
+          <div className='text-xl sm:text-2xl font-semibold'>{t('contactMe')}</div>
+          <a className='w-fit pt-2 sm:pt-4' href="mailto:jeanne.sabiron@prosae.net">jeanne.sabiron@prosae.net</a>
           <a href="tel:+33607045087" className='w-fit'>+33 6 07 04 50 87</a>
           <a href="https://instagram.com/_prosae_" className='flex flex-row items-center w-fit'>
             <div className='w-5 h-5 relative mr-2'>
@@ -53,8 +53,8 @@ export default function Footer(): JSX.Element {
           </a>
         </div>
       </div>
-      <div className='border-t-2 border-black mt-10' />
-      <div className='flex flex-row justify-between text-lg py-2 px-5'>
+      <div className='border-t-1 md:border-t-2 border-black mt-10' />
+      <div className='flex flex-row justify-between text-base sm:text-lg py-2 px-5'>
         <div>@2024 Prosae all rights reserved</div>
         <NavigationLink className="w-fit" href={'/privacy_policy'}>Mentions légales & CGV</NavigationLink>
       </div>
